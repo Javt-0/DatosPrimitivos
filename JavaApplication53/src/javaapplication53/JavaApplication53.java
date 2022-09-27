54cdc90a0f250ed655c43fe781976f6a5823b12f
Javt-0 <jonathan96aguilar@gmail.com>
2022-09-27 10:01:32 +0200

    Datos
    Todos los datos hechos

--- JavaApplication53/src/javaapplication53/JavaApplication53.java
+++ JavaApplication53/src/javaapplication53/JavaApplication53.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package javaapplication53;
 
 /**
@@ -10,11 +7,62 @@ package javaapplication53;
  */
 public class JavaApplication53 {
 
-    /**
-     * @param args the command line arguments
-     */
+    /*
+    Enteros:
+        - byte: 8 bits (-128 a 127) Byte.MIN_VALUE
+        - short: 16 bits (-32768 a 32767) Short.MIN_VALUE - short.MAX_VALUE
+        - int: 
+        - long:
+    
+    Decimales: 
+        - float: 
+        - double: 
+    
+    
+    
+    */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("\t****** NUMEROS ENTEROS ******");
+        Byte enteroByte = 5;
+        System.out.println("\nNumero entero byte: " + enteroByte);
+        System.out.println("Valor min del tipo byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor max del tipo byte: " + Byte.MAX_VALUE);
+        
+        Short enteroShort = 100;
+        System.out.println("\nNumero entero short: " + enteroShort);
+        System.out.println("Valor min del tipo short: " + Short.MIN_VALUE);
+        System.out.println("Valor max del tipo short: " + Short.MAX_VALUE);
+        
+        int enteroInt = 10000;
+        System.out.println("\nNumero entero int: " + enteroInt);
+        System.out.println("Valor min del tipo int: " + Integer.MIN_VALUE);
+        System.out.println("Valor max del tipo int: " + Integer.MAX_VALUE);
+        
+        long enteroLong = 1000000;
+        System.out.println("\nNumero entero long: " + enteroLong);
+        System.out.println("Valor min del tipo long: " + Long.MIN_VALUE);
+        System.out.println("Valor max del tipo long: " + Long.MAX_VALUE);
+        
+        System.out.println("\n\t****** NUMEROS DECIMALES ******");
+        float decimalFloat = 100;
+        System.out.println("\nNumero decimal float: " + decimalFloat/3);
+        System.out.println("Valor min del tipo float: " + Float.MIN_VALUE);
+        System.out.println("Valor max del tipo float: " + Float.MAX_VALUE);
+        
+        double decimalDouble = 10000;
+        System.out.println("\nNumero decimal double: " + decimalDouble/7);
+        System.out.println("Valor min del tipo double: " + Double.MIN_VALUE);
+        System.out.println("Valor max del tipo double: " + Double.MAX_VALUE);
+        
+        System.out.println("\n\t****** CARACTERES ******");
+        char caracter = 'f';
+        System.out.println("\nCaracter de tipo char: " + caracter);
+        
+        System.out.println("\n\t****** CADENA DE TEXTO ******");
+        String cadena = "Hola mundo";
+        System.out.println("\nCadena de texto de tipo String: " + cadena + "\n");
+        
+
     }
     
 }
